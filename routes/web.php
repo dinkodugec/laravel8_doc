abb4bf21ccfbd3b71001ee790fd9e62d39ce5e87
Dinko <dinko.dugec@gmail.com>
2022-11-06 16:57:57 +0100

    some basic of route; parameters, optional, naming

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function () {  //function() is anonymus function, often this is controller
     return view('welcome');
-});
+    /* return 'Home page'; string hrmr psgr in browser
+     */
+})->name('home.index');
+
+
+Route::get('/contact', function () {  //function() is anonymus function, often this is controller
+    return 'contact';
+    /* return 'Home page'; string home page in browser
+     */
+})->name('home.contact');
+
+Route::get('/posts/{id}', function ($id) {  
+    return 'Blog post' . $id;
+})/* ->where([
+    'id'=> '[0-9] +'
+]) */
+->name('posts.show');
+
+Route::get('/recent-posts/{days_ago?}' , function($daysAgo=20){
+    return 'Posts from ' . $daysAgo . ' days ago';
+})->name('posts.recent.index');
