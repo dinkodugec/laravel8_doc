f1a1f1184ac9324072781ac1495b30c3483a5693
Dinko <dinko.dugec@gmail.com>
2023-01-25 21:51:28 +0100

    using model factory inside Seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,5 +29,18 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
+
+
     }
+
+    public function defaultUser()
+    {
+
+        return $this->state([ 'name' => 'Dinko', 'email' => 'dinko.dugec@test.com' ]);
+
+    }
+
+
+
+
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Faker\Factory as FakerFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -16,12 +19,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        DB::table('users')->insert([
+      /*   DB::table('users')->insert([
             'name' => 'Dinko Dugc',
             'email' => 'dinko@laravel.test',
             'email_verified_at' => now(),
             'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
             'remember_token' => Str::random(10),
-        ]);
+        ]); */
+
+        User::factory()->defaultUser()->create();
+
+        User::factory(20)->create();
     }
+
+
+
 }
