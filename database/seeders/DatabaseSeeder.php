0185a6da3949238c143b739439c4499e48a7c32b
Dinko <dinko.dugec@gmail.com>
2023-01-25 22:57:32 +0100

    run factory in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\BlogPosts;
 use App\Models\User;
 use Faker\Factory as FakerFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+/* use Illuminate\Support\Facades\DB; */
 use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
@@ -25,11 +26,13 @@ class DatabaseSeeder extends Seeder
             'email_verified_at' => now(),
             'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
             'remember_token' => Str::random(10),
-        ]); */
+        ]); this is random bx new Laravel APP */
 
-        User::factory()->defaultUser()->create();
+       User::factory()->defaultUser()->create();
 
-        User::factory(20)->create();
+       User::factory(20)->create();
+
+
     }
 
 
