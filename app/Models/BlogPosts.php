af3901cdaa35150c6fc5277841d343b9fcfbaef0
Dinko <dinko.dugec@gmail.com>
2023-01-28 22:34:49 +0100

    soft deletes

--- app/Models/BlogPosts.php
+++ app/Models/BlogPosts.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPosts extends Model
 {
     protected $fillable = ['title', 'content'];
 
     use HasFactory;
+
+    use SoftDeletes;
 
 
     public function comments()
