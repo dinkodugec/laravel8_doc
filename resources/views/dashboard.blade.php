2b22eb4cc574d77ab9d4f9ccb32cdfc98e36e859
Dinko <dinko.dugec@gmail.com>
2023-09-12 23:40:50 +0200

    trnaslation folder, configure language ina pp, pasing data, plural forms

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <h1>{{ __('messages.welcome') }}</h1>
+                    <h1>@lang('messages.welcome')</h1>
+
+                    <p>{{ __('messages.example_with_value', ['name' => 'John']) }}</p>
+
+                    <p>{{ trans_choice('messages.plural', 0, ['a' => 1]) }}</p>
+                    <p>{{ trans_choice('messages.plural', 1, ['a' => 1]) }}</p>
+                    <p>{{ trans_choice('messages.plural', 2, ['a' => 1]) }}</p>
                 </div>
             </div>
         </div>
